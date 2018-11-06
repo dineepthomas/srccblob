b22f008b371fc1ae2876ff10d27d9abecd808876
Dineep Thomas <dthomas@cyclone.lsu.edu>
2018-11-06 14:57:37 -0600

    source files for blog

---
+++ content/mysecondpost.md
@@ -0,0 +1,11 @@
+Title: My Second post
+Date: 2017-11-26 10:01
+Modified: 2017-11-27 12:30
+Category: misc
+Tags: second, misc
+Slug: My-second-post
+Authors: SRCC
+Summary: What does your post talk about? Write here.
+
+This is the *second post* from my Pelican blog.
+![Photo]({attach}images/screenshot.png)
---
+++ pelicanconf.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'srcc members'
+SITENAME = u'SRCC blog'
+SITEURL = u'srccblog.lsu.edu/'
+
+PATH = 'content'
+
+TIMEZONE = 'US/Central'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+# CATEGORY_FEED_ATOM = None
+# TRANSLATION_FEED_ATOM = None
+# AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Blogroll
+LINKS = (('SRCC Home', 'https://www.srcc.lsu.edu/'),
+         ('SRCC"s CLIMATE EXTREMES PORTAL', 'http://extremes.srcc.lsu.edu/'),
+         ('Climdat', 'https://climdata.srcc.lsu.edu/'),
+        )
+
+# Feeds 
+FEEDS =  (('All posts', 'feeds/all.atom.xml'),
+          ('Category', 'feeds/category'),
+          ('OPW', 'feeds/tag/opw.atom.xml'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/srcclsu'),
+          ('Facebook', 'https://www.facebook.com/srcclsu'),)
+
+# Static path
+STATIC_PATHS= ['images']
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# Formatting for dates
+
+DEFAULT_DATE_FORMAT = ('%a %d %B %Y')
+
+# Formatting for urls
+
+ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{slug}/"
+ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{slug}/index.html"
+
+CATEGORY_URL = "category/{slug}"
+CATEGORY_SAVE_AS = "category/{slug}/index.html"
+
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = "tag/{slug}/index.html"
+
+# Generate yearly archive
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+
+# Show most recent posts first
+
+NEWEST_FIRST_ARCHIVES = False
+
+# Specify theme
+
+THEME = "bootstrap2"
